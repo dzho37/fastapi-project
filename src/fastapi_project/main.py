a46f1ee422bfb274863113ced24a6bf2a50b5c3d
Zhomart Zhumabekov <89572724+zhzhumabekov@users.noreply.github.com>
2026-09-16 23:43:42 +0500

    Creat route in main

--- src/fastapi_project/main.py
+++ src/fastapi_project/main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.get("/about")
+def about():
+    return {"message": "About page"}
+
+
+@app.get("/users")
+def users():
+    return {"message": "Users list"}
+
+
+@app.post("/users")
+def create_users():
+    return {"message": "User created"}
